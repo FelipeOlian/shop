6972917d00d515b64ab7919b396d181bb02e3ade
FelipeOlian <138076007+FelipeOlian@users.noreply.github.com>
2023-06-28 22:05:49 -0300

    Upload dos arquivos do site.

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggleBagItems(){
+    const bagItems = document.querySelector(".bag-items");
+
+    if(bagItems.style.display === "none"){
+        bagItems.style.display = "block";
+    } else {
+        bagItems.style.display = "none";
+    }
+   
+}
